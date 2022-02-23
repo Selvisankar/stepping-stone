3f70c03e1b5824df5caa9bc4edb6275c4598b025
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-02-23 15:19:15 +0530

    Add files via upload

---
+++ findintersection.java
@@ -0,0 +1,18 @@
+package assignments;
+
+public class findintersection {
+
+	public static void main(String[] args) {
+		int[] arr1= {3,2,11,4,6,7};
+		int[] arr2= {1,2,8,4,9,7};
+		for(int i=0;i<=arr1.length;i++)
+		{
+			if(arr1[i]==arr2[i]) {
+				System.out.println(arr1[i]);
+			}
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}

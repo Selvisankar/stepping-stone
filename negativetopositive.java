34f62a70172648d21087e672150628419538a963
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-02-23 14:57:12 +0530

    Add files via upload

---
+++ negativetopositive.java
@@ -0,0 +1,17 @@
+package assignments;
+
+public class negativetopositive {
+
+	public static void main(String[] args) {
+		int number=-40;
+		if(number<0) {
+			System.out.println("number is negative "+number +" it is converted to positive as:");
+			number=(-1*number);
+			
+			
+		}
+		// TODO Auto-generated method stub
+System.out.print(number);
+	}
+
+}

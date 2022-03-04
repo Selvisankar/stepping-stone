f53bbd9a47434a3a5d56c8454bb21f9263cb47e2
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-03-04 19:10:47 +0530

    Add files via upload

---
+++ login.java
@@ -0,0 +1,30 @@
+package seleniumsample;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class login {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[contains(text(),'Login')]")).click();
+		Thread.sleep(2000);
+		System.out.println("title of the page:"+driver.getTitle());
+		driver.findElement(By.xpath("//a[contains (text(),'Log Out')]")).click();
+		driver.close();
+		
+		
+		
+		
+		
+	}
+
+}

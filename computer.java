e3f64fee9cda0c5e789791a90e24acc92b519cbf
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-03-05 18:16:14 +0530

    Add files via upload

---
+++ computer.java
@@ -0,0 +1,12 @@
+package seleniumsample;
+
+public class computer {
+	public void computermodel() {
+		System.out.println("print the computermodel");
+	}
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+
+		}
+
+	}
---
+++ desktop.java
@@ -0,0 +1,17 @@
+package seleniumsample;
+
+
+
+	public class desktop extends computer {
+		public void desktopsize()
+		{
+		System.out.println("print the desktopsize");	
+		}
+			public static void main(String[] args) {
+				// TODO Auto-generated method stub
+		desktop obj=new desktop();
+		obj.computermodel();
+		obj.desktopsize();
+			}
+
+		}

394c6ceb7df51428dc7519ec6afd0daf553f9306
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-03-05 18:56:07 +0530

    Add files via upload

---
+++ axisbank.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class axisbank extends bankinfo{
+	public void deposit() {
+		System.out.println("fixed or recurring depopsit");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		axisbank override=new axisbank();
+		override.deposit();
+
+	}
+
+}
---
+++ bankinfo.java
@@ -0,0 +1,19 @@
+package assignments;
+
+public class bankinfo {
+	public void saving() {
+		System.out.println("saving ");
+	}
+	public void fixed() {
+		System.out.println("fixed");
+	}
+	public void deposit() {
+		System.out.println("deposit");
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

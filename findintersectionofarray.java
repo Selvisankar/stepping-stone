cac1da6020d17eff8ccec7f1d060539995e9367c
Selvisankar <100021278+Selvisankar@users.noreply.github.com>
2022-03-07 15:05:14 +0530

    Add files via upload

---
+++ findintersectionofarray.java
@@ -0,0 +1,24 @@
+package assignments;
+
+public class findintersectionofarray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int[] array1= {3,2,11,4,6,7};
+	int[] array2= {1,2,8,4,9,7};
+	System.out.println("the numbers that are matched in both arrays:");
+	for(int i=0;i<array1.length;i++)
+	{
+		for(int j=0;j<array2.length;j++) {
+		
+		if(array1[i]==array2[j])
+			System.out.println(array1[i]);
+		
+		}
+		}
+	}
+	
+	
+	}
+
+
